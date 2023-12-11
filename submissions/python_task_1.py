272240c7e553b1de982597a76107496b5515176a
Kiran S <138436939+Gvdz11@users.noreply.github.com>
2023-12-11 20:59:50 +0530

    Update python_task_1.py

--- submissions/python_task_1.py
+++ submissions/python_task_1.py
@@ -11,9 +11,15 @@ def generate_car_matrix(df)->pd.DataFrame:
     Returns:
         pandas.DataFrame: Matrix generated with 'car' values, 
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
-    """
-    # Write your logic here
+     # Pivot the DataFrame to create the desired matrix
+    car_matrix = df.pivot(index='id_1', columns='id_2', values='car')
 
+    # Fill NaN values with an empty string (you can change it based on your needs)
+    car_matrix = car_matrix.fillna('')
+
+    # Set diagonal values to 0
+    for idx in car_matrix.index:
+        car_matrix.loc[idx, idx] = 0
     return df
 
 
@@ -27,7 +33,8 @@ def get_type_count(df)->dict:
     Returns:
         dict: A dictionary with car types as keys and their counts as values.
     """
-    # Write your logic here
+    # Use value_counts to count occurrences of each car type
+    car_counts = df['car'].value_counts().to_dict()
 
     return dict()
 
@@ -42,7 +49,12 @@ def get_bus_indexes(df)->list:
     Returns:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
-    # Write your logic here
+     # Calculate the mean of 'bus' values
+    bus_mean = df['bus'].mean()
+
+    # Find indexes where 'bus' values exceed twice the mean
+    bus_indexes = df[df['bus'] > 2 * bus_mean].index.tolist()
+
 
     return list()
 
@@ -57,7 +69,11 @@ def filter_routes(df)->list:
     Returns:
         list: List of route names with average 'truck' values greater than 7.
     """
-    # Write your logic here
+    # Group by route and calculate the average 'truck' value for each route
+    route_averages = df.groupby('route')['truck'].mean()
+
+    # Filter routes where the average 'truck' value is greater than 7
+    filtered_routes = route_averages[route_averages > 7].index.tolist()
 
     return list()
 
@@ -72,7 +88,17 @@ def multiply_matrix(matrix)->pd.DataFrame:
     Returns:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
-    # Write your logic here
+    def custom_multiply(value):
+        # Define your custom conditions for multiplication
+        if value < 5:
+            return value * 2
+        elif value >= 5 and value < 10:
+            return value * 3
+        else:
+            return value
+
+    # Apply the custom function to each element in the matrix
+    modified_matrix = matrix.applymap(custom_multiply)
 
     return matrix
 
@@ -87,6 +113,11 @@ def time_check(df)->pd.Series:
     Returns:
         pd.Series: return a boolean series
     """
-    # Write your logic here
+     # Assuming your DataFrame has a 'timestamp' column
+    # Convert the 'timestamp' column to datetime
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+    # Group by 'id' and 'id_2' and check if each group covers a full 24-hour and 7 days period
+    completeness_check = df.groupby(['id', 'id_2'])['timestamp'].agg(lambda x: x.max() - x.min() == pd.Timedelta(days=7, hours=24))
 
     return pd.Series()
